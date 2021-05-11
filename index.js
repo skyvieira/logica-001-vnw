d7ae80134ec330f715699dbb19118c985bc56d50
liviavieira <vieira.liviaaa@gmail.com>
2021-05-11 10:22:51 -0300

    meowmeow

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+const name = 'Ana'
+let age = 17
+let year = 2021
+const human = false
+
+const num1 = 3
+const num2 = 3
+
+const cat = 'meow'
+
+document.write(cat)
+
+console.log(name)
+console.log('Meu ano de nascimento é:', year - age - 1)
+console.log('Você é um humano?', human)
+console.log(num1 + num2)
